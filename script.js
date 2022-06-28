fd4c07b35f3376ec21d61a7ed7369ef9ce0bb338
sultan <sultanhasanov987@gmail.com>
2022-06-28 23:32:32 +0300

    Добавил стили

---
+++ script.js
@@ -0,0 +1,38 @@
+const input = document.getElementById('input')
+const btn = document.getElementById('btn')
+const result = document.getElementById('result')
+const total = document.getElementById('total')
+let i = 0;
+
+btn.addEventListener('click', (e) => {
+    // const li = document.createElement('li')
+    if(input.value === '') return
+    createElementDelete(input.value)
+    input.value = ''
+
+})
+
+function createElementDelete(value) {
+    i++
+    const li = document.createElement('li')
+    li.className = 'li'
+    li.textContent = value
+
+    const btn2 = document.createElement('button')
+    btn2.textContent = ' X '
+    li.appendChild(btn2)
+
+    btn2.addEventListener ('click', (e) => {
+        i--
+        total.textContent = i  
+        result.removeChild(li)
+    })
+
+    li.addEventListener ('click', (e) => {
+        li.classList.toggle('li-active')
+    })
+
+    total.textContent = i
+
+    result.appendChild(li)
+}
