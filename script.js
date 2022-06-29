d6a981caac573539992870832eb7045fefd388e8
sultan <sultanhasanov987@gmail.com>
2022-06-29 13:25:51 +0300

    Добавил чекбокс

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const input = document.getElementById('input')
 const btn = document.getElementById('btn')
 const result = document.getElementById('result')
 const total = document.getElementById('total')
-let i = 0;
+let count = 0;
 
 btn.addEventListener('click', (e) => {
     // const li = document.createElement('li')
-    if(input.value === '') return
+    if(input.value === '' || count === 13) return
     createElementDelete(input.value)
     input.value = ''
 
 })
 
 function createElementDelete(value) {
-    i++
+    count++
     const li = document.createElement('li')
     li.className = 'li'
     li.textContent = value
@@ -24,16 +24,20 @@ function createElementDelete(value) {
     li.appendChild(btn2)
 
     btn2.addEventListener ('click', (e) => {
-        i--
-        total.textContent = i  
+        count--
+        total.textContent = count  
         result.removeChild(li)
     })
 
-    li.addEventListener ('click', (e) => {
+    const input2 = document.createElement("input");
+    input2.setAttribute("type", "checkbox");
+    li.prepend(input2)
+    
+    input2.addEventListener ('click', (e) => {
         li.classList.toggle('li-active')
     })
 
-    total.textContent = i
+    total.textContent = count
 
     result.appendChild(li)
 }
